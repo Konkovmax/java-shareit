8bd3863097c054154619cf1d390c270eb0d2a9dd
Konkovmax <konkovmax@yandex.ru>
2022-11-08 17:28:19 +0300

    all done, after refactor

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -20,9 +20,10 @@ public class Item {
     private int id;
     private String name;
     private String description;
+    @Column(name = "is_available")
     private Boolean available;
     @ManyToOne()
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "owner_id")
     private User owner;
     @ManyToOne()
     @JoinColumn(name = "request_id")
